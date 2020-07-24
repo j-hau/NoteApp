556e2f6ea7e46514474fead196eeaa44cade79ff
James <james.hau@vybn.co.uk>
2020-07-24 14:31:56 +0100

    Tests and code for non-zero guessed words

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -9,6 +9,37 @@ const GuessedWords = (props) => {
                 Try to guess the secret word!
             </span>
         )
+    } else {
+        const guessedWordsRows = props.guessedWords.map((word, index) => (
+            <tr data-test="guessed-word" key={index}>
+                <td>
+                    {word.guessedWord}
+                </td>
+                <td>
+                    {word.letterMatchCount}
+                </td>
+            </tr>
+        ))
+        contents = (
+           <div data-test="guessed-words">
+               <h3> Guessed words </h3>
+               <table>
+                   <thead>
+                       <tr>
+                           <th>
+                               Guess
+                           </th>
+                           <th>
+                               Matching Letters
+                           </th>
+                       </tr>
+                       <tbody>
+                           { guessedWordsRows }
+                       </tbody>
+                   </thead>
+               </table>
+           </div> 
+        )
     }
     return(
         <div data-test="component-guessed-words">
